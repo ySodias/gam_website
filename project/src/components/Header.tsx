25773bb960a2d4a50fe74d0e8b6dd57bd06e643b
ySodias <ySodia@gmail.com>
2022-02-20 11:33:57 -0300

    create header

---
+++ project/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import {Navbar, Container, Row, Col} from 'react-bootstrap';
+import Vector from '../assets/Vector.svg';
+import Logo from '../assets/Logo.svg'
+import CSS from 'csstype';
+
+const barStyle: CSS.Properties = {
+  height: '70%',
+  width: '70%'
+}
+const logoStyle: CSS.Properties = {
+  height: '100%',
+  width: '100%'
+}
+
+export function Header(){
+  return (
+    <Navbar bg="light" variant="light">
+      <Container>
+      <Navbar.Brand href="#home"><img className='fluid' style={barStyle} src={Vector} /></Navbar.Brand>
+      <Row>
+        <Col><img className='fluid' style={logoStyle} src={Logo}/></Col>
+        <Col></Col>
+      </Row>
+      </Container>
+    </Navbar>
+  )
+}
